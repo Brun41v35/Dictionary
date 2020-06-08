14fd1fa6e3e20bace03632004c7a25b1da1fe3dc
Bruno Alves <alves.bruno945@gmail.com>
2020-06-08 16:27:13 -0300

    Dictionary
    A example dictionary in swift

---
+++ Dicionario.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+// Exemplo de dicionário em Swift
+var states = [
+    "SP" : "Sao Paulo",
+    "RJ" : "Rio de Janeiro",
+    "GO" : "Goias"
+]
+
+// Exemplo de dicionario vazio
+var listaMedia: [String : Double] = [:]
+print(states)
